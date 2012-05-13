624da089c5b867772fd87308797834e236311ef1
Arthur Endsley <kaendsle@mtu.edu>
2012-05-13 18:13:41 -0400

    Added the Data and Instrumentation pages

--- public/views.py
+++ public/views.py
@@ -18,3 +18,17 @@ def display_about(request):
     localhost/gass/about/
     '''
     return render_to_response('about.html', {})
+
+
+def display_access(request):
+    '''
+    localhost/gass/access/
+    '''
+    return render_to_response('data.html', {})
+
+
+def display_instruments(request):
+    '''
+    localhost/gass/hardware/
+    '''
+    return render_to_response('instruments.html', {})
